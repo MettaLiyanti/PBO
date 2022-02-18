6d0c94ded118e7a135a86ae236c54056ee59424b
MettaLiyanti <mettaliyanti@uvers.ac.id>
2022-02-18 23:02:52 +0700

    Pertemuan 03

---
+++ Pertemuan 03/3_1/Variables01_Example/src/variables01/Variables01.java
@@ -0,0 +1,26 @@
+
+package variables01;
+
+public class Variables01 {
+
+    public static void main(String[] args) {
+        String x = "Enka";
+        System.out.println("Haloo!!! Namaku  " +x);
+        System.out.println(x +" adalah seorang anime lovers!");
+        System.out.println("Yo, " +x +"!");
+        System.out.println("Ogenki desu ka, " +x +"-san? "
+                + "Yosh! My name is " +x +". I know how excited "
+                + "everyone is to start appreciating " +x +" on " +x
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay "
+                + x +"!!! That's me! " +x +" is the best date ever!");
+        
+        String nama = "Alex";
+        nama = "Kira";
+        nama = "Aizawa";
+        
+        //"Yuki" = nama; // can not do this
+        System.out.println(nama);
+        
+    }
+    
+}
