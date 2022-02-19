63bdc34d99bdfd58a4024827396dccf911032325
MettaLiyanti <mettaliyanti@uvers.ac.id>
2022-02-19 19:21:46 +0700

    Pertemuan 03

---
+++ Pertemuan 03/3_3/DataTekstual/src/datatekstual/DataTekstual.java
@@ -0,0 +1,69 @@
+package datatekstual;
+
+public class DataTekstual {
+
+    public static void main(String[] args) {
+        
+//        char shirt3Size = 'L'; // char utk single character, pakai tanda kutip 1
+//        String shirt6Size = "XXXL"; // String utk multiple character, pakai tanda kutip 2
+        
+//        // Menggabungkan String
+//        String sapa1 = "Halo";
+//        String sapa2 = "Dunia";
+//        String pesan1 = sapa1+" "+sapa2+"!";
+//            System.out.println(pesan1); // Halo Dunia!
+//        // atau
+//            System.out.println(sapa1+" "+sapa2+"!"); // Halo Dunia!
+        
+//        // Menggabungkan String dan Angka
+//        String totalHarga1 = "Total: Rp"+5000;
+//          System.out.println(totalHarga1); // Total: Rp 5000
+//        // Harus hati-hati
+//        String totalHarga2 = "Total: Rp"+5000+1000; // Total: Rp50001000
+//          System.out.println(totalHarga2);
+//        // Pakai tanda kurung utk angkanya
+//        String totalHarga3 = "Total: Rp"+(5000+1000);
+//          System.out.println(totalHarga3); // Total: Rp6000
+        
+//        // Escape Sequence
+//        System.out.println("Insert\t new\ttab"); // Insert	 new	tab
+//        System.out.println("In\bsert backspace\b"); // Isert backspac
+//        System.out.println("Insert new \nline"); // Insert new
+//        // line
+//        System.out.println("In\rsert carriage return"); // sert carriage return
+//        System.out.println("\fInsert formfe\fed"); // Insert formfeed
+//        System.out.println("Insert single quote char" + '\''); // Insert single quote char'
+//        System.out.println("Insert \"double\" quote char"); // Insert "double" quote char
+//        System.out.println("Insert \\backslash ch\\aracter"); // Insert \backslash ch\aracter
+        
+//        // Print Statement
+//        System.out.println("Ini baris pertama."
+//            + "Ini BUKAN baris kedua.");
+//        /* output: 
+//        Ini baris pertama.Ini BUKAN baris kedua.
+//        */
+//        System.out.println("Ini baris pertama.\n"
+//            + "Ini baris kedua.");
+//        /* output: 
+//        Ini baris pertama.
+//        Ini baris kedua.
+//        */
+        
+        // pakai println()
+        System.out.println("Baris pertama println");
+        System.out.println("Baris kedua println");
+        /* output: 
+        Baris pertama println
+        Baris kedua println
+        */
+        // pakai print()
+        System.out.print("Baris pertama print.");
+        System.out.print("BUKAN baris kedua print");
+        /* output: 
+        Baris pertama print.BUKAN baris kedua print
+        */
+        
+        
+    }
+    
+}
