eca46b7e7292238d31b8fdc1460b8547189f6b04
MettaLiyanti <mettaliyanti@uvers.ac.id>
2022-02-26 18:09:43 +0700

    Pertemuan 04

--- Pertemuan 04/4_2/ImportEx/src/com/example/AddImport.java
+++ Pertemuan 04/4_2/ImportEx/src/com/example/AddImport.java
@@ -1,7 +1,6 @@
 
 package com.example;
 
-//import java.util.Calendar; // salah package
 import java.util.Date;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
@@ -9,18 +8,15 @@ import javax.swing.JFrame;
 public class AddImport {
 
     public static void main(String args[]) {
-        /*javax.swing.*/JLabel label = new /*javax.swing.*/JLabel("Hello");
-        
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(100,100);
-        frame.setVisible(true);
-        frame.add(label);
+//        /*javax.swing.*/JLabel label = new /*javax.swing.*/JLabel("Hello");
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(100,100);
+//        frame.setVisible(true);
+//        frame.add(label);
         
         Date tanggal = new Date();
         System.out.println(tanggal.getDate());
-        Calender kalender = new Calender();
-        System.out.println(kalender.getClass());
         
 
     }
